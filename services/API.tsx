796a57bceebd5d452a17a437cb1b8f9295a055f4
Shaganpreet-kaur <shaganpreetk632@gmail.com>
2025-03-27 09:33:52 -0600

    tabs

--- services/API.tsx
+++ services/API.tsx
@@ -1,19 +1,12 @@
-const API_KEY = "YOUR_API_KEY";
+const API_KEY = " 1d04f720c29b4685af1143558252703"; // Replace with your actual WeatherAPI key
+const BASE_URL = "https://api.weatherapi.com/v1";
 
 const WeatherService = {
+  // Fetch current weather using WeatherAPI
   getCurrentWeather: async (city: string): Promise<any> => {
-    const response = await fetch(
-      `https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city}&aqi=no`
-    );
+    const response = await fetch(`${BASE_URL}/current.json?key=${API_KEY}&q=${city}&aqi=no`);
     return response.json();
   },
-
-  getWeatherForecast: async (city: string): Promise<any> => {
-    const response = await fetch(
-      `https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${city}&days=3&aqi=no`
-    );
-    return response.json();
-  }
 };
 
 export default WeatherService;
